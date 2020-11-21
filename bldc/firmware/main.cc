0e0b42020589e91e2e82e8cbcc3bc53208b972ee
Tim Blakely <tim.blakely@gmail.com>
2020-11-20 21:42:56 -0800

    Added flash

--- bldc/firmware/main.cc
+++ bldc/firmware/main.cc
@@ -1,16 +1,20 @@
 #include <stdint.h>
 
-// #include "third_party/stm32cubeg4/stm32g474xx.h"
-
+#include "bldc/firmware/stm32g474/drivers/flash.h"
 #include "bldc/firmware/stm32g474/drivers/fpu.h"
 #include "bldc/firmware/stm32g474/drivers/rcc.h"
 
+using stm32g474::drivers::Flash;
 using stm32g474::drivers::Fpu;
 using stm32g474::drivers::Rcc;
 
 int main() {
   Fpu::EnableHardwareFPU();
   Rcc::SetupClocks();
+
+  Flash::EnableDataCache();
+  Flash::EnableInstructionCache();
+  Flash::EnablePrefetchBuffer();
   while (true) {
     asm("nop");
   }
