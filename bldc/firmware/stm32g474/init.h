576cb60ca3ada825de8ad8de5a9239c84136948e
Tim Blakely <tim.blakely@gmail.com>
2020-11-28 23:37:35 -0800

    Removing old files

--- bldc/firmware/stm32g474/init.h
+++
@@ -1,8 +0,0 @@
-#ifndef BLDC_FIRMWARE_STM32G474_MCU_CONFIG_H_
-#define BLDC_FIRMWARE_STM32G474_MCU_CONFIG_H_
-
-namespace stm32g474 {
-void Startup();
-}  // namespace stm32g474
-
-#endif  // BLDC_FIRMWARE_STM32G474_MCU_CONFIG_H_
