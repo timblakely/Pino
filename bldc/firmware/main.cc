e6b0225c49267248569231f67cb32527a04fc635
Tim Blakely <tim.blakely@gmail.com>
2020-11-28 20:42:23 -0800

    Sequestered startup info

--- bldc/firmware/main.cc
+++ bldc/firmware/main.cc
@@ -1,21 +1,15 @@
-#include "bldc/firmware/stm32g474/drivers/flash.h"
-#include "bldc/firmware/stm32g474/drivers/fpu.h"
 #include "bldc/firmware/stm32g474/drivers/gpio.h"
-#include "bldc/firmware/stm32g474/drivers/nvic.h"
-#include "bldc/firmware/stm32g474/drivers/rcc.h"
 #include "bldc/firmware/stm32g474/drivers/timer.h"
+#include "bldc/firmware/stm32g474/init.h"
 #include "bldc/firmware/stm32g474/led.h"
 
 using stm32g474::Led;
-using stm32g474::drivers::CortexInterrupt;
-using stm32g474::drivers::Flash;
-using stm32g474::drivers::Fpu;
+
 using stm32g474::drivers::Gpio;
 using stm32g474::drivers::GpioOutputMode;
 using stm32g474::drivers::GpioPort;
 using stm32g474::drivers::GpioPullup;
-using stm32g474::drivers::Nvic;
-using stm32g474::drivers::Rcc;
+
 using stm32g474::drivers::SysTickTimer;
 
 Led kRedLED(GpioPort::B, 6);
@@ -23,20 +17,7 @@ Led kGreenLED(GpioPort::B, 9);
 Led kBlueLED(GpioPort::B, 7);
 
 int main() {
-  Fpu::EnableHardwareFPU();
-  Nvic::Init([] {
-    // Default handler.
-    while (true) {
-      asm("nop");  //
-    }
-  });
-
-  Rcc::SetupClocks();
-
-  Flash::EnableDataCache();
-  Flash::EnableInstructionCache();
-  Flash::EnablePrefetchBuffer();
-
+  stm32g474::Startup();
   kRedLED.On();
 
   uint32_t i = 0;
---
+++ bldc/firmware/stm32g474/init.h
@@ -0,0 +1,8 @@
+#ifndef BLDC_FIRMWARE_STM32G474_MCU_CONFIG_H_
+#define BLDC_FIRMWARE_STM32G474_MCU_CONFIG_H_
+
+namespace stm32g474 {
+void Startup();
+}  // namespace stm32g474
+
+#endif  // BLDC_FIRMWARE_STM32G474_MCU_CONFIG_H_
