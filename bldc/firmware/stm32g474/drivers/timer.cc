67d0f85fde77f5708801184fe49f23170e352e5c
Tim Blakely <tim.blakely@gmail.com>
2020-12-01 00:06:56 -0800

    Got timer working

--- bldc/firmware/stm32g474/drivers/timer.cc
+++ bldc/firmware/stm32g474/drivers/timer.cc
@@ -74,12 +74,13 @@ void Tim3::Configure() {
   // Timer-wide settings
   LL_TIM_SetClockDivision(TIM3, LL_TIM_CLOCKDIVISION_DIV1);
   LL_TIM_SetPrescaler(TIM3, 17000);
-  LL_TIM_SetAutoReload(TIM3, 10000);
+  const uint32_t period = 10000;
+  LL_TIM_SetAutoReload(TIM3, period);
   LL_TIM_SetRepetitionCounter(TIM3, 0);
 
   // CH4 settings.
   LL_TIM_CC_EnableChannel(TIM3, LL_TIM_CHANNEL_CH4);
-  LL_TIM_OC_SetCompareCH4(TIM3, 5000);
+  LL_TIM_OC_SetCompareCH4(TIM3, period * .5);
   LL_TIM_OC_ConfigOutput(TIM3, LL_TIM_CHANNEL_CH4,
                          LL_TIM_OCIDLESTATE_LOW | LL_TIM_OCPOLARITY_HIGH);
   // LL_TIM_OC_SetPolarity(TIM3, LL_TIM_CHANNEL_CH4, LL_TIM_OCPOLARITY_HIGH);
