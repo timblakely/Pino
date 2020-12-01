094293df43bad843da825121bf1c35c5df53b812
Tim Blakely <tim.blakely@gmail.com>
2020-11-30 17:16:03 -0800

    Converted to pstd

--- bldc/firmware/stm32g474/drivers/gpio.cc
+++ bldc/firmware/stm32g474/drivers/gpio.cc
@@ -15,14 +15,15 @@ struct Gpio::Pin::GPIO_TypeDefI : GPIO_TypeDef {};
 
 Gpio::Pin::Pin(Port port, uint32_t pin)
     : port_(port), pin_(pin), ll_pin_(1U << pin) {
-  constexpr static const auto port_map =
-      pstd::ConstexprMap<Port, std::intptr_t>({
-          {Port::A, GPIOA_BASE},
-          {Port::B, GPIOB_BASE},
-          {Port::C, GPIOC_BASE},
-          {Port::D, GPIOD_BASE},
-          {Port::E, GPIOE_BASE},
-      });
+  // Have to use GPIOx_BASE here, since the actual GPIOx is a reinterpret_cast,
+  // which isn't allowed in constexpr.
+  static const auto port_map = pstd::ConstexprMap<Port, std::intptr_t>({
+      {Port::A, GPIOA_BASE},
+      {Port::B, GPIOB_BASE},
+      {Port::C, GPIOC_BASE},
+      {Port::D, GPIOD_BASE},
+      {Port::E, GPIOE_BASE},
+  });
   ll_port_ = reinterpret_cast<GPIO_TypeDefI*>(port_map.at(port));
 }
 
@@ -49,42 +50,30 @@ void Gpio::Pin::Configure(OutputMode mode, Pullup pullup, AlternateFunction af,
 }
 
 void Gpio::Pin::SetMode(OutputMode mode) {
-  switch (mode) {
-    case OutputMode::OpenDrain:
-      return LL_GPIO_SetPinOutputType(ll_port_, ll_pin_,
-                                      LL_GPIO_OUTPUT_OPENDRAIN);
-    case OutputMode::PushPull:
-      return LL_GPIO_SetPinOutputType(ll_port_, ll_pin_,
-                                      LL_GPIO_OUTPUT_PUSHPULL);
-  }
+  static const auto mode_map = pstd::ConstexprMap<OutputMode, std::intptr_t>({
+      {OutputMode::OpenDrain, LL_GPIO_OUTPUT_OPENDRAIN},
+      {OutputMode::PushPull, LL_GPIO_OUTPUT_PUSHPULL},
+  });
+  LL_GPIO_SetPinOutputType(ll_port_, ll_pin_, mode_map.at(mode));
 }
 
 void Gpio::Pin::SetPullup(Pullup pullup) {
-  switch (pullup) {
-    case Pullup::PullUp:
-      LL_GPIO_SetPinPull(ll_port_, ll_pin_, LL_GPIO_PULL_UP);
-      break;
-    case Pullup::PullDown:
-      LL_GPIO_SetPinPull(ll_port_, ll_pin_, LL_GPIO_PULL_DOWN);
-      break;
-    case Pullup::None:
-      LL_GPIO_SetPinPull(ll_port_, ll_pin_, LL_GPIO_PULL_NO);
-      break;
-  }
+  static const auto pull_map = pstd::ConstexprMap<Pullup, uint32_t>({
+      {Pullup::None, LL_GPIO_PULL_NO},
+      {Pullup::PullUp, LL_GPIO_PULL_UP},
+      {Pullup::PullDown, LL_GPIO_PULL_DOWN},
+  });
+  LL_GPIO_SetPinPull(ll_port_, ll_pin_, pull_map.at(pullup));
 }
 
 void Gpio::Pin::SetSpeed(Speed speed) {
-  switch (speed) {
-    case Speed::Low:
-      return LL_GPIO_SetPinSpeed(ll_port_, ll_pin_, LL_GPIO_SPEED_FREQ_LOW);
-    case Speed::Medium:
-      return LL_GPIO_SetPinSpeed(ll_port_, ll_pin_, LL_GPIO_SPEED_FREQ_MEDIUM);
-    case Speed::High:
-      return LL_GPIO_SetPinSpeed(ll_port_, ll_pin_, LL_GPIO_SPEED_FREQ_HIGH);
-    case Speed::VeryHigh:
-      return LL_GPIO_SetPinSpeed(ll_port_, ll_pin_,
-                                 LL_GPIO_SPEED_FREQ_VERY_HIGH);
-  }
+  static const auto speed_map = pstd::ConstexprMap<Speed, uint32_t>({
+      {Speed::Low, LL_GPIO_SPEED_FREQ_LOW},
+      {Speed::Medium, LL_GPIO_SPEED_FREQ_MEDIUM},
+      {Speed::High, LL_GPIO_SPEED_FREQ_HIGH},
+      {Speed::VeryHigh, LL_GPIO_SPEED_FREQ_VERY_HIGH},
+  });
+  return LL_GPIO_SetPinSpeed(ll_port_, ll_pin_, speed_map.at(speed));
 }
 
 void Gpio::Pin::High() { LL_GPIO_SetOutputPin(ll_port_, ll_pin_); }
