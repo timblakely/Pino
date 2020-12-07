77b47530ad68b54f640ceffa94a2e164939f0d94
Tim Blakely <tim.blakely@gmail.com>
2020-12-06 19:45:03 -0800

    Per-platform config

--- bldc/firmware/platform/nucleo/setup.cc
+++ bldc/firmware/platform/nucleo/setup.cc
@@ -1,6 +1,51 @@
+#include "bldc/firmware/platform/system.h"
+#include "bldc/firmware/stm32g474/drivers/flash.h"
+#include "bldc/firmware/stm32g474/drivers/fpu.h"
+#include "bldc/firmware/stm32g474/drivers/gpio.h"
+#include "bldc/firmware/stm32g474/drivers/nvic.h"
+#include "bldc/firmware/stm32g474/drivers/rcc.h"
+#include "bldc/firmware/stm32g474/led.h"
 
+using stm32g474::Led;
+using stm32g474::drivers::Flash;
+using stm32g474::drivers::Fpu;
+using stm32g474::drivers::Gpio;
+using stm32g474::drivers::Nvic;
+using stm32g474::drivers::Rcc;
+
+using stm32g474::drivers::Interrupt;
 
 namespace platform {
 
-void Setup() { printf("Setting up nucleo version"); }
+Led kUserLed({Gpio::Port::A, 5});
+
+void Platform::Startup() {
+  Fpu::EnableHardwareFPU();
+  Nvic::Init(/* Default interrupt handler */ [] {
+    while (true) {
+      asm("nop");  //
+    }
+  });
+  // Note: Might need to come after clock setup.
+  Flash::EnableDataCache();
+  Flash::EnableInstructionCache();
+  Flash::EnablePrefetchBuffer();
+
+  Rcc::SetupClocks();
+
+  Nvic::SetInterruptHandler(Interrupt::HardFault, [] {
+    kUserLed.On();
+    while (true) {
+      asm("nop");
+    }
+  });
+}
+
+void Platform::Fatal() {
+  // Intentionally read the beef!
+  volatile auto deadbeef = *(volatile uint32_t *)0xdeadbeef;
+  // Get rid of compiler warning.
+  (void)deadbeef;
+}
+
 }  // namespace platform
