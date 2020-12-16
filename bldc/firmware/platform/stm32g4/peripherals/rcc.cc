5f0901d374aab3d1e84e450f47fb3d3db5fabaa5
Tim Blakely <tim.blakely@gmail.com>
2020-12-15 18:27:05 -0800

    Removed namespaces

--- bldc/firmware/platform/stm32g4/peripherals/rcc.cc
+++ bldc/firmware/platform/stm32g4/peripherals/rcc.cc
@@ -4,18 +4,9 @@
 #include "bldc/firmware/platform/stm32g4/peripherals/timer.h"
 #include "third_party/mapbox/eternal/eternal.hpp"
 #include "third_party/stm32cubeg4/stm32g474xx.h"
-
-namespace bus {
 #include "third_party/stm32cubeg4/stm32g4xx_ll_bus.h"
-}  // namespace bus
-
-namespace pwr {
 #include "third_party/stm32cubeg4/stm32g4xx_ll_pwr.h"
-}  // namespace pwr
-
-namespace rcc {
 #include "third_party/stm32cubeg4/stm32g4xx_ll_rcc.h"
-}  // namespace rcc
 
 namespace platform {
 namespace stm32g4 {
@@ -25,54 +16,54 @@ void Rcc::SetupClocks() {
 
   // TODO(blakely): This isn't technically relatd to RCC, but it's still
   // important to do early.
-  pwr::LL_PWR_DisableUCPDDeadBattery();
+  LL_PWR_DisableUCPDDeadBattery();
 
-  bus::LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
-  bus::LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
 
   // Allow for bumping the core voltage by 80mV during high frequency operation.
-  pwr::LL_PWR_EnableRange1BoostMode();
+  LL_PWR_EnableRange1BoostMode();
 
   // Configure HSE
-  rcc::LL_RCC_HSE_Enable();
-  while (!rcc::LL_RCC_HSE_IsReady()) {
+  LL_RCC_HSE_Enable();
+  while (!LL_RCC_HSE_IsReady()) {
     asm("nop");
   }
 
   // Don't use HSI
-  rcc::LL_RCC_HSI_Disable();
+  LL_RCC_HSI_Disable();
 
   // Configure PLL
-  rcc::LL_RCC_PLL_Disable();
-  while (rcc::LL_RCC_PLL_IsReady()) {
+  LL_RCC_PLL_Disable();
+  while (LL_RCC_PLL_IsReady()) {
     asm("nop");
   }
-  rcc::LL_RCC_PLL_SetMainSource(LL_RCC_PLLSOURCE_HSE);
-  rcc::LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_6, 85,
-                                   LL_RCC_PLLR_DIV_2);
-  rcc::LL_RCC_PLL_Enable();
-  while (!rcc::LL_RCC_PLL_IsReady()) {
+  LL_RCC_PLL_SetMainSource(LL_RCC_PLLSOURCE_HSE);
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_6, 85,
+                              LL_RCC_PLLR_DIV_2);
+  LL_RCC_PLL_Enable();
+  while (!LL_RCC_PLL_IsReady()) {
     asm("nop");
   }
-  rcc::LL_RCC_PLL_EnableDomain_SYS();
+  LL_RCC_PLL_EnableDomain_SYS();
 
   // Set the system clock source to HSE. However, we can't ramp directly from
   // the 16MHz HSI directly to 170MHz. Have to enter a few intermediate states
   // first:
 
   // 1) Set AHB prescalar div to 2 (=8Mhz)
-  rcc::LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_2);
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_2);
 
   // 2) Lower the peripheral clocks all the way down so they don't freak out
   //    during transition.
-  rcc::LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_16);
-  rcc::LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_16);
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_16);
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_16);
 
   // 3) Set Clock source to PLL (170/2=85Mhz)
-  rcc::LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
 
   // 4) Wait for sysclksource to change
-  while (rcc::LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL) {
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL) {
     asm("nop");
   }
 
@@ -84,24 +75,24 @@ void Rcc::SetupClocks() {
   SysTickTimer::BlockingWait(1);
 
   // 7) Set AHB prescalar div to 1 (=170Mhz) (done below)
-  rcc::LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
 
   // Reset system clock again to match new divider.
   SysTickTimer::UpdatePeriod();
 
   // Configure peripheral clocks back to where they're supposed to be.
-  rcc::LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
-  rcc::LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
 
   // TODO(blakely): move into their respective drivers
   // Peripheral clock configuration
-  rcc::LL_RCC_SetLPUARTClockSource(LL_RCC_LPUART1_CLKSOURCE_PCLK1);
-  rcc::LL_RCC_SetADCClockSource(LL_RCC_ADC12_CLKSOURCE_SYSCLK);
-  rcc::LL_RCC_SetADCClockSource(LL_RCC_ADC345_CLKSOURCE_SYSCLK);
+  LL_RCC_SetLPUARTClockSource(LL_RCC_LPUART1_CLKSOURCE_PCLK1);
+  LL_RCC_SetADCClockSource(LL_RCC_ADC12_CLKSOURCE_SYSCLK);
+  LL_RCC_SetADCClockSource(LL_RCC_ADC345_CLKSOURCE_SYSCLK);
 }
 
 uint32_t Rcc::GetSysClockFrequency() {
-  switch (rcc::LL_RCC_GetSysClkSource()) {
+  switch (LL_RCC_GetSysClkSource()) {
     case LL_RCC_SYS_CLKSOURCE_STATUS_HSI:
       return HSI_VALUE;
       break;
@@ -111,51 +102,47 @@ uint32_t Rcc::GetSysClockFrequency() {
       break;
     case LL_RCC_SYS_CLKSOURCE_STATUS_PLL:
     default:
-      return __LL_RCC_CALC_PLLCLK_FREQ(
-          kHSEOscillatorSpeed, rcc::LL_RCC_PLL_GetDivider(),
-          rcc::LL_RCC_PLL_GetN(), rcc::LL_RCC_PLL_GetR());
+      return __LL_RCC_CALC_PLLCLK_FREQ(kHSEOscillatorSpeed,
+                                       LL_RCC_PLL_GetDivider(),
+                                       LL_RCC_PLL_GetN(), LL_RCC_PLL_GetR());
   }
 }
 
 uint32_t Rcc::GetHClockFrequency() {
   const auto sysclock_freq = GetSysClockFrequency();
-  const auto ahb_prescalar = rcc::LL_RCC_GetAHBPrescaler();
+  const auto ahb_prescalar = LL_RCC_GetAHBPrescaler();
   return __LL_RCC_CALC_HCLK_FREQ(sysclock_freq, ahb_prescalar);
 }
 
 void Rcc::Enable(Gpio::Port port) {
   switch (port) {
     case Gpio::Port::A:
-      return bus::LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
+      return LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
     case Gpio::Port::B:
-      return bus::LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB);
+      return LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB);
     case Gpio::Port::C:
-      return bus::LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOC);
+      return LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOC);
     case Gpio::Port::D:
-      return bus::LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOD);
+      return LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOD);
     case Gpio::Port::E:
-      return bus::LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOE);
+      return LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOE);
   }
 }
 
 // void Rcc::Enable(AdvancedTimer timer) {
 //   switch (timer) {
 //     case AdvancedTimer::Tim1:
-//       return bus::LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_TIM1);
+//       return LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_TIM1);
 //     case AdvancedTimer::Tim8:
-//       return bus::LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_TIM8);
+//       return LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_TIM8);
 //     case AdvancedTimer::Tim20:
-//       return bus::LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_TIM20);
+//       return LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_TIM20);
 //   }
 // }
 
-void Rcc::EnableTim3() {
-  bus::LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM3);
-}
+void Rcc::EnableTim3() { LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM3); }
 
-void Rcc::EnableTim2() {
-  bus::LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
-}
+void Rcc::EnableTim2() { LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2); }
 
 }  // namespace stm32g4
 }  // namespace platform
