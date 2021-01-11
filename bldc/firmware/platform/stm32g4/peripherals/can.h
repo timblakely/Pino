f1b0334af9854fcfbbd18142f2c572782a2f95b7
Tim Blakely <tim.blakely@gmail.com>
2021-01-11 14:46:08 -0800

    Added test register

--- bldc/firmware/platform/stm32g4/peripherals/can.h
+++ bldc/firmware/platform/stm32g4/peripherals/can.h
@@ -18,19 +18,19 @@ struct CanInstance {
   // Core release register
   CRELRegister CREL;
 
-  // Endian register
   struct ENDNRegister {
     uint32_t EVT;
     operator uint32_t() { return reinterpret_cast<uint32_t>(EVT); }
   };
+  // Endian register
   ENDNRegister ENDN;
 
-  // DO NOT USE CURRENTLY BROKEN HACKHACKHACK
   struct DBTPRegister {
     enum class TransceiverDelayCompensation : bool {
       Disable = 0,
       Enable = 1,
     };
+
     uint8_t DSJW : 4;
     uint8_t DTSEG2 : 4;
     uint8_t DTSEG1 : 5;
@@ -39,8 +39,34 @@ struct CanInstance {
     unsigned : 2;  // Reserved
     TransceiverDelayCompensation TDC : 1;
   };
-
+  // Data bit timing and prescalar
   DBTPRegister DBTP;
+
+  struct TESTRegister {
+    enum class LoopbackMode : bool {
+      Disabled = 0,
+      Enabled = 1,
+    };
+
+    enum class TransmitPin : uint8_t {
+      Default = 0b00U,      // Normal operation; updated at end of CAN bit time.
+      SamplePoint = 0b01U,  // Updated at sample point.
+      Dominant = 0b10U,     // Dominant (0) level at ping FDCANx_TX
+      Recessive = 0b11U,    // Recessive
+    };
+
+    enum class ReceivePin : uint8_t {
+      Dominant = 0b0U,
+      Recessive = 0b1U,
+    };
+
+    unsigned : 4;  // Reserved
+    LoopbackMode LBCK : 1;
+    TransmitPin TX : 2;
+    ReceivePin RX : 1;
+  };
+  // Test mode. Must be enabled by setting bit `CCCR[TEST]` to 1.
+  TESTRegister TEST;
 };
 
 }  // namespace internal
