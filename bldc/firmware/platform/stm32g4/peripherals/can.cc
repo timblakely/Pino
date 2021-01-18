b489b997626273a7fe5c267d61d8e48963f1691c
Tim Blakely <tim.blakely@gmail.com>
2021-01-17 21:50:53 -0800

    Pretty major refactor

--- bldc/firmware/platform/stm32g4/peripherals/can.cc
+++ bldc/firmware/platform/stm32g4/peripherals/can.cc
@@ -7,11 +7,149 @@
 namespace platform {
 namespace stm32g4 {
 
+namespace can {
+
+// Ensure memory addresses are at expected locations.
+static_assert(sizeof(Periph) == 0x104);
+static_assert(sizeof(StandardFilter) == 1 * 4);
+static constexpr uint32_t kStandardFilterMemOffset = 0x0;
+static_assert(kStandardFilterMemOffset == 0x0000);
+static_assert(sizeof(ExtendedFilter) == 2 * 4);
+static constexpr uint32_t kExtendedFilterMemOffset =
+    kStandardFilterMemOffset + sizeof(StandardFilter) * 28;
+static_assert(kExtendedFilterMemOffset == 0x0070);
+static_assert(sizeof(RxBuffer) == 18 * 4);
+static constexpr uint32_t kRxFIFO0MemOffset =
+    kExtendedFilterMemOffset + sizeof(ExtendedFilter) * 8;
+static_assert(kRxFIFO0MemOffset == 0x00B0);
+static constexpr uint32_t kRxFIFO1MemOffset =
+    kRxFIFO0MemOffset + sizeof(RxBuffer) * 3;
+static_assert(kRxFIFO1MemOffset == 0x0188);
+static_assert(sizeof(TxEvent) == 2 * 4);
+static constexpr uint32_t kTxEventFifoMemOffset =
+    kRxFIFO1MemOffset + sizeof(RxBuffer) * 3;
+static_assert(kTxEventFifoMemOffset == 0x0260);
+static_assert(sizeof(TxBuffer) == 18 * 4);
+static constexpr uint32_t kTxBufferMemOffset =
+    kTxEventFifoMemOffset + sizeof(TxEvent) * 3;
+static_assert(kTxBufferMemOffset == 0x0278);
+
+// Final memory accounting
+static_assert(kMRAMBankSize ==
+              (sizeof(StandardFilter) * 28 + sizeof(ExtendedFilter) * 8 +
+               sizeof(RxBuffer) * 3 + sizeof(RxBuffer) * 3 +
+               sizeof(TxEvent) * 3 + sizeof(TxBuffer) * 3));
+
+// Implementation
+
+void Periph::InitMode() {
+  // Enter init mode.
+  update_CCCR([](CCCR v) { return v.with_INIT(CCCR::INIT_t::init); });
+  // Wait for ACK to init mode, in case we're already transmitting.
+  while (read_CCCR().get_INIT() == CCCR::INIT_t::run) {
+  }
+  // Enable config writing
+  update_CCCR([](CCCR v) { return v.with_CCE(CCCR::CCE_t::readwrite); });
+}
+
+void Periph::Initialize() {
+  update_CCCR([](CCCR v) {
+    return v
+        // Enable Tx pause
+        .with_TXP(1)
+        // No edge filtering
+        .with_EFBI(0)
+        // Protocol exception handling disalbed
+        .with_PXHD(0)
+        // TODO(blakely): Enable bit rate switching eventually.
+        // No bit rate switching
+        .with_BRSE(0)
+        // Enable FDCAN mode
+        .with_FDOE(1)
+        // No test mode
+        .with_TEST(CCCR::TEST_t::normal)
+        // Enable automatic retransmission
+        .with_DAR(CCCR::DAR_t::retransmit)
+        // No bus monitoring
+        .with_MON(0)
+        // No restricted mode
+        .with_ASM(CCCR::ASM_t::normal)
+        // Make sure we're not in sleep mode.
+        .with_CSR(0);
+  });
+}
+
+void Periph::SetBitTiming() {
+  update_NBTP([](NBTP v) {
+    // TODO(blakely): Pull actual clock source and freq from RCC. This
+    // assumes PCLK1@170MHz, aiming for 1MBit
+
+    // NBRP = 6
+    // NTSEG1 = 11
+    // NTSEG2 = 10
+    // NSJW = 4
+
+    // tq = (NBRP + 1) * t_fdcan_clk = (6 + 1) * 5.88235ns = 41.176ns
+    // t_syncseg = 1 * tq = 41.176ns
+    // bs1 = tq * (NTSEG1 + 1) = 41.176ns * (11 + 1) = 494.41 ns
+    // bs2 = tq * (NTSEG2 + 1) = 41.176ns * (10 + 1) = 452.94 ns
+    // Bit time = 41.176 + 494.41 + 452.94 = 988.235 ns
+    // Baud = 1e9 / (988.235) = 1.0116 MBit
+
+    return v.with_NBRP(6)
+        .with_NTSEG1(11)
+        .with_NTSEG2(10)
+        // Jump width of 4
+        .with_NSJW(4);
+  });
+}
+
+void Periph::EnableQueueMode() {
+  update_TXBC([](TXBC v) { return v.with_TFQM(TXBC::TFQM_t::fifo); });
+}
+
+void Periph::SetClockDivider() {
+  update_CKDIV([](CKDIV v) { return v.with_PDIV(CKDIV::PDIV_t::div1); });
+}
+
+void Periph::EnableLoopbackMode() {
+  update_CCCR([](CCCR v) {
+    return v.with_TEST(CCCR::TEST_t::test)
+        .with_MON(0)
+        .with_ASM(CCCR::ASM_t::normal);
+  });
+  update_TEST([](TEST v) { return v.with_LBCK(1); });
+}
+
+void Periph::Start() {
+  update_CCCR([](CCCR v) { return v.with_INIT(CCCR::INIT_t::run); });
+}
+
+uint8_t Periph::TxPut() { return read_TXFQS().get_TFQPI(); }
+
+uint8_t Periph::TxGet() { return read_TXFQS().get_TFGI(); }
+
+}  // namespace can
+
+using can::ExtendedFilter;
+using can::kExtendedFilterMemOffset;
+using can::kMRAMAddress;
+using can::kMRAMBankSize;
+using can::kRxFIFO0MemOffset;
+using can::kRxFIFO1MemOffset;
+using can::kTxBufferMemOffset;
+using can::kTxEventFifoMemOffset;
+using can::Periph;
+using can::RxBuffer;
+using can::StandardFilter;
+using can::TxBuffer;
+using can::TxEvent;
+
 Can::Can(Gpio::Pin tx, Gpio::Pin rx) : tx_(tx), rx_(rx) {}
 
 void Can::Init(Can::Instance instance) {
   instance_ = instance;
-  can_ = reinterpret_cast<Periph*>(instance);
+  can_ = reinterpret_cast<can::Periph*>(instance);
   switch (instance) {
     case Instance::Fdcan1:
       standard_filters_ = reinterpret_cast<StandardFilter*>(kMRAMAddress);
@@ -53,108 +191,19 @@ void Can::Init(Can::Instance instance) {
   // TODO(blakely): Do we need to read FDCAN_ENDN?
 
   // Init and control configuration.
-  {
-    using CCCR = Can::Periph::CCCR_value_t;
-    // TODO(blakely): Is this staged start necessary, or can we write CCCR INT,
-    // CCE, and other bits all at once?
-    // First, Update CCCR to enter init mode.
-    can_->update_CCCR([](CCCR v) { return v.with_INIT(CCCR::INIT_t::init); });
-
-    // Wait for ACK to init mode, in case we're already transmitting.
-    while (can_->read_CCCR().get_INIT() == CCCR::INIT_t::run) {
-    }
-
-    // Next, enable configuration readwrite.
-    can_->update_CCCR(
-        [](CCCR v) { return v.with_CCE(CCCR::CCE_t::readwrite); });
-
-    // Now set the various CCCR fields.
-    can_->update_CCCR([](CCCR v) {
-      return v
-          // Enable Tx pause
-          .with_TXP(1)
-          // No edge filtering
-          .with_EFBI(0)
-          // Protocol exception handling disalbed
-          .with_PXHD(0)
-          // TODO(blakely): Enable bit rate switching eventually.
-          // No bit rate switching
-          .with_BRSE(0)
-          // Enable FDCAN mode
-          .with_FDOE(1)
-          // No test mode
-          .with_TEST(CCCR::TEST_t::normal)
-          // Enable automatic retransmission
-          .with_DAR(CCCR::DAR_t::retransmit)
-          // No bus monitoring
-          .with_MON(0)
-          // No restricted mode
-          .with_ASM(CCCR::ASM_t::normal)
-          // Make sure we're not in sleep mode.
-          .with_CSR(0);
-    });
-  }
-
-  // Configure bit timing.
-  {
-    using NBTP = Can::Periph::NBTP_value_t;
-    can_->update_NBTP([](NBTP v) {
-      // TODO(blakely): Pull actual clock source and freq from RCC. This
-      // assumes PCLK1@170MHz, aiming for 1MBit
-
-      // NBRP = 6
-      // NTSEG1 = 11
-      // NTSEG2 = 10
-      // NSJW = 4
-
-      // tq = (NBRP + 1) * t_fdcan_clk = (6 + 1) * 5.88235ns = 41.176ns
-      // t_syncseg = 1 * tq = 41.176ns
-      // bs1 = tq * (NTSEG1 + 1) = 41.176ns * (11 + 1) = 494.41 ns
-      // bs2 = tq * (NTSEG2 + 1) = 41.176ns * (10 + 1) = 452.94 ns
-      // Bit time = 41.176 + 494.41 + 452.94 = 988.235 ns
-      // Baud = 1e9 / (988.235) = 1.0116 MBit
-
-      return v.with_NBRP(6)
-          .with_NTSEG1(11)
-          .with_NTSEG2(10)
-          // Jump width of 4
-          .with_NSJW(4);
-    });
-  }
-
+  can_->InitMode();
+  can_->Initialize();
+  can_->SetBitTiming();
+  can_->EnableQueueMode();
+  can_->EnableLoopbackMode();
   // TODO(blakely): Set DBTP. 8.01MBit is 2/2/2
-
-  // Configure for TxFIFO usage
-  {
-    using TXBC = Can::Periph::TXBC_value_t;
-    can_->update_TXBC([](TXBC v) { return v.with_TFQM(TXBC::TFQM_t::fifo); });
-  }
-
-  // Set clock divider
-  {
-    using CKDIV = Can::Periph::CKDIV_value_t;
-    can_->update_CKDIV(
-        [](CKDIV v) { return v.with_PDIV(CKDIV::PDIV_t::div1); });
-  }
+  can_->SetClockDivider();
 
   // TODO(blakely): Configure this though an enum to Init()
-  // Disable loopback mode
-  // {
-
-  {
-    using CCCR = Can::Periph::CCCR_value_t;
-    using TEST = Can::Periph::TEST_value_t;
-    can_->update_CCCR([](CCCR v) {
-      return v.with_TEST(CCCR::TEST_t::test)
-          .with_MON(0)
-          .with_ASM(CCCR::ASM_t::normal);
-    });
-    can_->update_TEST([](TEST v) { return v.with_LBCK(1); });
-  }
 
   // Set the first filter.
   {
-    using FLSSA = Can::StandardFilter::FLSSA_value_t;
+    using FLSSA = can::StandardFilter::FLSSA_value_t;
     standard_filters_[0].update_FLSSA([](FLSSA v) {
       return v.with_SFT(FLSSA::SFT_t::dual_id)
           .with_SFEC(FLSSA::SFEC_t::store_fifo0)
@@ -162,24 +211,15 @@ void Can::Init(Can::Instance instance) {
           .with_SFID2(37);
     });
   }
-
-  int i = 0;
-
-  ++i;
-  // Start up the FDCAN device
-  {
-    using CCCR = Can::Periph::CCCR_value_t;
-    can_->update_CCCR([](CCCR v) { return v.with_INIT(CCCR::INIT_t::run); });
-  }
-  ++i;
+  can_->Start();
 }
 
 void Can::TransmitData(uint8_t* data, uint8_t size) {
-  const auto idx = can_->tx_put();
+  const auto idx = can_->TxPut();
   auto buffer = &(tx_buffer_[idx]);
 
   {
-    using T0 = Can::TxBuffer::T0_value_t;
+    using T0 = can::TxBuffer::T0_value_t;
     buffer->update_T0([](T0 v) {
       return v
           // ESI only on error active
@@ -194,7 +234,7 @@ void Can::TransmitData(uint8_t* data, uint8_t size) {
   }
 
   {
-    using T1 = Can::TxBuffer::T1_value_t;
+    using T1 = can::TxBuffer::T1_value_t;
     buffer->update_T1([](T1 v) {
       return v
           // Set message marker
@@ -236,7 +276,7 @@ void Can::TransmitData(uint8_t* data, uint8_t size) {
 
   // Notify periphal of transmit request
   {
-    using TXBAR = Can::Periph::TXBAR_value_t;
+    using TXBAR = can::Periph::TXBAR_value_t;
     can_->update_TXBAR([&idx](TXBAR v) { return v.with_AR(1 << idx); });
   }
   int i = 0;
--- bldc/firmware/platform/stm32g4/peripherals/can.h
+++ bldc/firmware/platform/stm32g4/peripherals/can.h
@@ -6,6 +6,83 @@
 namespace platform {
 namespace stm32g4 {
 
+namespace can {
+
+#define PSL_INL inline
+
+static constexpr uint32_t kMRAMAddress = 0x4000'A400U;
+static constexpr uint32_t kMRAMBankSize = 0x350U /* 212*4=848 bytes */;
+
+// Registers
+struct Periph {
+#define ETL_BFF_DEFINITION_FILE \
+  "bldc/firmware/platform/stm32g4/peripherals/can_registers.inl"
+#include "third_party/etl/biffield/generate.h"
+#undef ETL_BFF_DEFINITION_FILE
+
+  PSL_INL void InitMode();
+  PSL_INL void EnableQueueMode();
+  PSL_INL void EnableLoopbackMode();
+  PSL_INL void Start();
+
+  PSL_INL void Transmit();
+
+  // TODO(blakely): Actually make these configurable?
+  PSL_INL void Initialize();
+  PSL_INL void SetBitTiming();
+  PSL_INL void SetClockDivider();
+
+  PSL_INL uint8_t TxPut();
+  PSL_INL uint8_t TxGet();
+
+ private:
+  using CCCR = CCCR_value_t;
+  using CKDIV = CKDIV_value_t;
+  using NBTP = NBTP_value_t;
+  using TEST = TEST_value_t;
+  using TXBC = TXBC_value_t;
+};
+
+// Memory configuration
+struct StandardFilter {
+#define ETL_BFF_DEFINITION_FILE \
+  "bldc/firmware/platform/stm32g4/peripherals/can_standard_filter_memory.inl"
+#include "third_party/etl/biffield/generate.h"
+#undef ETL_BFF_DEFINITION_FILE
+};
+
+struct ExtendedFilter {
+#define ETL_BFF_DEFINITION_FILE \
+  "bldc/firmware/platform/stm32g4/peripherals/can_extended_filter_memory.inl"
+#include "third_party/etl/biffield/generate.h"
+#undef ETL_BFF_DEFINITION_FILE
+};
+
+struct RxBuffer {
+#define ETL_BFF_DEFINITION_FILE \
+  "bldc/firmware/platform/stm32g4/peripherals/can_rx_buffer_memory.inl"
+#include "third_party/etl/biffield/generate.h"
+#undef ETL_BFF_DEFINITION_FILE
+  uint32_t data[16];
+};
+
+struct TxEvent {
+#define ETL_BFF_DEFINITION_FILE \
+  "bldc/firmware/platform/stm32g4/peripherals/can_tx_event_memory.inl"
+#include "third_party/etl/biffield/generate.h"
+#undef ETL_BFF_DEFINITION_FILE
+};
+
+struct TxBuffer {
+#define ETL_BFF_DEFINITION_FILE \
+  "bldc/firmware/platform/stm32g4/peripherals/can_tx_buffer_memory.inl"
+#include "third_party/etl/biffield/generate.h"
+#undef ETL_BFF_DEFINITION_FILE
+  uint32_t data[16];
+};
+
+}  // namespace can
+
 class Can {
  public:
   enum class Instance : uint32_t {
@@ -27,94 +104,14 @@ class Can {
   Gpio::Pin tx_;
   Gpio::Pin rx_;
 
-  // Registers
-  struct Periph {
-#define ETL_BFF_DEFINITION_FILE \
-  "bldc/firmware/platform/stm32g4/peripherals/can_registers.inl"
-#include "third_party/etl/biffield/generate.h"
-#undef ETL_BFF_DEFINITION_FILE
+  can::Periph* can_;
 
-    uint8_t tx_put() { return read_TXFQS().get_TFQPI(); }
-    uint8_t tx_get() { return read_TXFQS().get_TFGI(); }
-  };
-  static_assert(sizeof(Periph) == 0x104);
-  Periph* can_;
-
-  // Memory configuration
-
-  struct StandardFilter {
-#define ETL_BFF_DEFINITION_FILE \
-  "bldc/firmware/platform/stm32g4/peripherals/can_standard_filter_memory.inl"
-#include "third_party/etl/biffield/generate.h"
-#undef ETL_BFF_DEFINITION_FILE
-  };
-  static_assert(sizeof(StandardFilter) == 1 * 4);
-  StandardFilter* standard_filters_;
-  static constexpr uint32_t kStandardFilterMemOffset = 0x0;
-  static_assert(kStandardFilterMemOffset == 0x0000);
-
-  struct ExtendedFilter {
-#define ETL_BFF_DEFINITION_FILE \
-  "bldc/firmware/platform/stm32g4/peripherals/can_extended_filter_memory.inl"
-#include "third_party/etl/biffield/generate.h"
-#undef ETL_BFF_DEFINITION_FILE
-  };
-  static_assert(sizeof(ExtendedFilter) == 2 * 4);
-  ExtendedFilter* extended_filters_;
-  static constexpr uint32_t kExtendedFilterMemOffset =
-      kStandardFilterMemOffset + sizeof(StandardFilter) * 28;
-  static_assert(kExtendedFilterMemOffset == 0x0070);
-
-  struct RxBuffer {
-#define ETL_BFF_DEFINITION_FILE \
-  "bldc/firmware/platform/stm32g4/peripherals/can_rx_buffer_memory.inl"
-#include "third_party/etl/biffield/generate.h"
-#undef ETL_BFF_DEFINITION_FILE
-    uint32_t data[16];
-  };
-  static_assert(sizeof(RxBuffer) == 18 * 4);
-  RxBuffer* rx_fifo0_;
-  static constexpr uint32_t kRxFIFO0MemOffset =
-      kExtendedFilterMemOffset + sizeof(ExtendedFilter) * 8;
-  static_assert(kRxFIFO0MemOffset == 0x00B0);
-
-  RxBuffer* rx_fifo1_;
-  static constexpr uint32_t kRxFIFO1MemOffset =
-      kRxFIFO0MemOffset + sizeof(RxBuffer) * 3;
-  static_assert(kRxFIFO1MemOffset == 0x0188);
-
-  struct TxEvent {
-#define ETL_BFF_DEFINITION_FILE \
-  "bldc/firmware/platform/stm32g4/peripherals/can_tx_event_memory.inl"
-#include "third_party/etl/biffield/generate.h"
-#undef ETL_BFF_DEFINITION_FILE
-  };
-  static_assert(sizeof(TxEvent) == 2 * 4);
-  TxEvent* tx_event_fifo_;
-  static constexpr uint32_t kTxEventFifoMemOffset =
-      kRxFIFO1MemOffset + sizeof(RxBuffer) * 3;
-  static_assert(kTxEventFifoMemOffset == 0x0260);
-
-  struct TxBuffer {
-#define ETL_BFF_DEFINITION_FILE \
-  "bldc/firmware/platform/stm32g4/peripherals/can_tx_buffer_memory.inl"
-#include "third_party/etl/biffield/generate.h"
-#undef ETL_BFF_DEFINITION_FILE
-    uint32_t data[16];
-  };
-  static_assert(sizeof(TxBuffer) == 18 * 4);
-  TxBuffer* tx_buffer_;
-  static constexpr uint32_t kTxBufferMemOffset =
-      kTxEventFifoMemOffset + sizeof(TxEvent) * 3;
-  static_assert(kTxBufferMemOffset == 0x0278);
-  // Final memory accounting
-
-  static constexpr uint32_t kMRAMAddress = 0x4000'A400U;
-  static constexpr uint32_t kMRAMBankSize = 0x350U /* 212*4=848 bytes */;
-  static_assert(kMRAMBankSize ==
-                (sizeof(StandardFilter) * 28 + sizeof(ExtendedFilter) * 8 +
-                 sizeof(RxBuffer) * 3 + sizeof(RxBuffer) * 3 +
-                 sizeof(TxEvent) * 3 + sizeof(TxBuffer) * 3));
+  can::StandardFilter* standard_filters_;
+  can::ExtendedFilter* extended_filters_;
+  can::RxBuffer* rx_fifo0_;
+  can::RxBuffer* rx_fifo1_;
+  can::TxEvent* tx_event_fifo_;
+  can::TxBuffer* tx_buffer_;
 };
 
 }  // namespace stm32g4
