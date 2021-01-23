a21c510cd68a642c51474228d8c386ce23499bfe
Tim Blakely <tim.blakely@gmail.com>
2021-01-23 13:55:39 -0800

    Breaking out fields

--- tools/gen_biffields.py
+++ tools/gen_biffields.py
@@ -36,14 +36,15 @@ class Formatter:
 
   def __exit__(self, *args):
     self.tabs -= 2
-    if self.tabs < 0:
-      file.write('// clang-format on')
-      self.file.close()
+
+  def finish(self):
+    self.file.write('// clang-format on\n')
+    self.file.close()
 
   def write(self, msg=''):
     if self.file is None:
       self.file = open(self.path, 'w')
-      self.file.write('// clang-format off')
+      self.file.write('// clang-format off\n')
     if len(msg):
       self.file.write(f'{" " * self.tabs}{msg}\n')
     else:
@@ -63,7 +64,39 @@ class BiffieldBase:
     return self.fmt
 
 
-class BiffieldRegister(BiffieldBase):
+class FieldWriter(BiffieldBase):
+
+  def __init__(self, *args, **kwargs):
+    super(FieldWriter, self).__init__(*args, **kwargs)
+
+  def write(self, field):
+    if field.bit_width == 1:
+      dtype = 'bool'
+    elif field.bit_width <= 8:
+      dtype = 'uint8_t'
+    elif field.bit_width <= 16:
+      dtype = 'uint16_t'
+    else:
+      dtype = 'uint32_t'
+    start = field.bit_offset
+    end = field.bit_offset + field.bit_width - 1
+    if not field.is_enumerated_type:
+      self.fmt.write(f'ETL_BFF_FIELD({end}:{start}, {dtype}, {field.name})')
+    else:
+      self.fmt.write(f'ETL_BFF_FIELD_E({end}:{start}, {dtype}, {field.name},')
+      with self.section:
+        self.write_enumerated_values(field, field.enumerated_values)
+      self.fmt.write(f')')
+
+  def write_enumerated_values(self, field, values):
+    for v in values:
+      bit_str = f'{v.value:b}'
+      if len(bit_str) < field.bit_width:
+        bit_str = '0' * (field.bit_width - len(bit_str)) + bit_str
+      self.fmt.write(f'ETL_BFF_ENUM(0b{bit_str}, {v.name})')
+
+
+class RegWriter(BiffieldBase):
   size_map = {
       32: 'uint32_t',
       16: 'uint16_t',
@@ -77,61 +110,63 @@ class BiffieldRegister(BiffieldBase):
       'write-only': 'ETL_BFF_REG_WO',
   }
 
-  def __init__(self, descriptions, reserved_comments, *args, **kwargs):
-    super(BiffieldRegister, self).__init__(*args, **kwargs)
-    self.description = descriptions
-    self.reserved_comments = reserved_comments
+  def __init__(self, *args, **kwargs):
+    super(RegWriter, self).__init__(*args, **kwargs)
+    self.fieldwriter = FieldWriter(self.fmt)
 
   def _write_description(self, register):
-    if not self.description:
+    if not FLAGS.register_descriptions:
       return
     description = re.sub(' +', ' ', register.description)
     description = textwrap.fill(
         description, width=80, initial_indent='// ', subsequent_indent='// ')
     self.fmt.write(description)
 
+  def _write_reserved(self, field):
+    if self.last_bit is None:
+      self.last_bit = field.bit_offset
+      return
+
+    high_bit = (field.bit_offset + field.bit_width - 1)
+    gap = self.last_bit - high_bit - 1
+    if gap > 0:
+      if FLAGS.reserved_comments:
+        if gap == 1:
+          reserved_str = f'// {self.last_bit-1} reserved'
+        else:
+          reserved_str = f'// {self.last_bit-1} : {high_bit+1} reserved'
+      else:
+        reserved_str = ''
+      self.fmt.write(reserved_str)
+    self.last_bit = field.bit_offset
+
   def write(self, register):
+    self.last_bit = None
     self._write_description(register)
 
     size = self.size_map[register._size]
     reg_type = self.reg_type[register._access]
     self.fmt.write(f'{reg_type}({size}, {register.name},')
     with self.section:
-      last_bit = None
-      for idx, f in enumerate(
-          sorted(register.fields, key=lambda x: x.bit_offset, reverse=True)):
-        if last_bit is not None:
-          high_bit = (f.bit_offset + f.bit_width - 1)
-          gap = last_bit - high_bit - 1
-          if gap > 0:
-            if self.reserved_comments:
-              if gap == 1:
-                reserved_str = f'// {last_bit-1} reserved'
-              else:
-                reserved_str = f'// {last_bit-1} : {high_bit+1} reserved'
-            else:
-              reserved_str = ''
-            self.fmt.write(reserved_str)
-        # self.write_field(f)
-        last_bit = f.bit_offset
+      for f in sorted(
+          register.fields, key=lambda x: x.bit_offset, reverse=True):
+        self._write_reserved(f)
+        self.fieldwriter.write(f)
     self.fmt.write(')')
 
 
 class Biffile:
 
-  def __init__(self, output_path, register_descriptions, reserved_comments):
-    self.formatter = Formatter(output_path)
-    self.regwriter = BiffieldRegister(register_descriptions, reserved_comments,
-                                      self.formatter)
+  def __init__(self, formatter):
+    self.fmt = formatter
+    self.regwriter = RegWriter(self.fmt)
 
     # self.output_path = output_path
     self.num_reserved = 0
-    self.reg_desc = register_descriptions
-    self.reserved_comments = reserved_comments
 
   @property
   def section(self):
-    return self.formatter
+    return self.fmt
 
   def write_peripheral(self, peripheral):
     next_addr = 0
@@ -141,46 +176,18 @@ class Biffile:
         reserved_size = (r.address_offset - next_addr)
         self.write_reserved_register(reserved_size)
       self.write_register(r)
-      self.formatter.newln()
+      self.fmt.newln()
       next_addr = r.address_offset + 4
 
   def write_reserved_register(self, reserved_size):
-    self.formatter.write(
+    self.fmt.write(
         f'ETL_BFF_REG_RESERVED(uint32_t, reserved{self.num_reserved+1}, {reserved_size//4})'
     )
-    self.formatter.newln()
+    self.fmt.newln()
     self.num_reserved += 1
 
   def write_register(self, register):
     self.regwriter.write(register)
-
-  def write_field(self, field):
-    if field.bit_width == 1:
-      dtype = 'bool'
-    elif field.bit_width <= 8:
-      dtype = 'uint8_t'
-    elif field.bit_width <= 16:
-      dtype = 'uint16_t'
-    else:
-      dtype = 'uint32_t'
-    start = field.bit_offset
-    end = field.bit_offset + field.bit_width - 1
-    if not field.is_enumerated_type:
-      self.formatter.write(
-          f'ETL_BFF_FIELD({end}:{start}, {dtype}, {field.name})')
-    else:
-      self.formatter.write(
-          f'ETL_BFF_FIELD_E({end}:{start}, {dtype}, {field.name},')
-      with self.section:
-        self.write_enumerated_values(field, field.enumerated_values)
-      self.formatter.write(f')')
-
-  def write_enumerated_values(self, field, values):
-    for v in values:
-      bit_str = f'{v.value:b}'
-      if len(bit_str) < field.bit_width:
-        bit_str = '0' * (field.bit_width - len(bit_str)) + bit_str
-      self.formatter.write(f'ETL_BFF_ENUM(0b{bit_str}, {v.name})')
 
 
 def main(unused_argv):
@@ -196,8 +203,9 @@ def main(unused_argv):
   ][0]
   # dest_file = open(FLAGS.output_path, 'w')
 
-  Biffile(FLAGS.output_path, FLAGS.register_descriptions,
-          FLAGS.reserved_comments).write_peripheral(fdcan1)
+  formatter = Formatter(FLAGS.output_path)
+  Biffile(formatter).write_peripheral(fdcan1)
+  formatter.finish()
 
   # print('%s @ 0x%08x' % (fdcan1.name,fdcan1.base_address))
   # for r in fdcan1.registers:
